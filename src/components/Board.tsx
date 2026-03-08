a6c58233fe4685f36c204c1a36d1c76485cce489
Tamelab <TAMELAB@HAWAII.EDU>
2026-03-07 22:27:26 -1000

    Challenge 1 - basic component structure

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,26 +1,11 @@
 import Square from "./Square";
 
-type Props = {
-  squares: string[];
-  onClick: (i: number) => void;
-};
-
-export default function Board({ squares, onClick }: Props) {
-
-  function renderSquare(i: number) {
-    return (
-      <Square
-        value={squares[i]}
-        onClick={() => onClick(i)}
-      />
-    );
-  }
-
+export default function Board() {
   return (
     <div>
-      <div>{renderSquare(0)}{renderSquare(1)}{renderSquare(2)}</div>
-      <div>{renderSquare(3)}{renderSquare(4)}{renderSquare(5)}</div>
-      <div>{renderSquare(6)}{renderSquare(7)}{renderSquare(8)}</div>
+      <Square />
+      <Square />
+      <Square />
     </div>
   );
 }
